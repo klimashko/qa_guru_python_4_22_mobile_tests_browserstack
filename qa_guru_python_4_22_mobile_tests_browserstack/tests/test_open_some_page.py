77fa8893e7ab6462b8579002d70b3a00aa1c092d
evgenii klimashko <klimashko108@gmail.com>
2023-05-19 03:52:40 +0300

    Тест broserstack_sample.py работает, сой тест пока нет, Тесты в одном файле - надо вынести данные и фикстуры

---
+++ qa_guru_python_4_22_mobile_tests_browserstack/tests/test_open_some_page.py
@@ -0,0 +1,46 @@
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+from appium.webdriver.common.appiumby import AppiumBy
+from selene import have, be
+from selene.support.shared import browser
+
+
+
+
+def test_open_searching_page():
+    # Options are only available since client version 2.3.0
+    # If you use an older client then switch to desired_capabilities
+    # instead: https://github.com/appium/python-client/pull/720
+    options = UiAutomator2Options().load_capabilities({
+        # Specify device and os_version for testing
+        "platformName": "android",
+        "platformVersion": "9.0",
+        "deviceName": "Google Pixel 3",
+
+        # Set URL of the application under test
+        "app": "bs://sample.app",
+
+        # Set other BrowserStack capabilities
+        'bstack:options': {
+            "projectName": "First Python project",
+            "buildName": "browserstack-build-EK-MOBILE-DEMO",
+            "sessionName": "BStack first_test",
+
+            # Set your access credentials
+            "userName": "evgenyklimashko_HCYP2F",
+            "accessKey": "9khU39uhcNny1pmfajTg"
+        }
+    })
+
+    # Initialize the remote Webdriver using BrowserStack remote URL
+    browser.config.driver = webdriver.Remote("http://hub.browserstack.com/wd/hub", options=options)
+
+    # Test case for the BrowserStack sample Android app.
+    browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+
+    browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type("Toledo, Spain")
+
+    # browser.element((AppiumBy.ACCESSIBILITY_ID, "Toledo, Spain")).should(be.visible).click()
+    browser.element((AppiumBy.CLASS_NAME, "android.widget.TextView")).should(have.text("Toledo, Spain"))
+
+    browser.quit()
